52430d05b718ae22a17d682d0d284f6ed70dea5a
Mikael Nyvelius <mikael.nyvelius@betssongroup.com>
2015-12-02 14:11:04 +0100

    Work in progress

--- otto.ino
+++ otto.ino
@@ -9,13 +9,28 @@ ZumoReflectanceSensorArray reflectanceSensors;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 
+int lastError = 0;
+
 const int MAX_SPEED = 100;
 
 void setup() {
   reflectanceSensors.init();
   button.waitForButton();
+  motors.setSpeeds(0,0);
+  Serial.begin(9600);
+
+  motors.setSpeeds(MAX_SPEED, -MAX_SPEED);
+  
+  
+  for (int i = 0; i < 360; i++) {  
+    reflectanceSensors.calibrate();
+    delay(12);
+  }
+  motors.setSpeeds(0, 0);
 }
 
 void loop() {
-  motors.setSpeeds(MAX_SPEED, MAX_SPEED);
+  unsigned int sensors[6];
+  int position = reflectanceSensors.readLine(sensors);
+  Serial.println(String(position));
 }
