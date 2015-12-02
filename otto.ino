6b9548a58d178625518efc112046771fd95ca0dd
Mikael Nyvelius <mikael.nyvelius@betssongroup.com>
2015-12-02 15:01:09 +0100

    Detect when on line

--- otto.ino
+++ otto.ino
@@ -15,13 +15,15 @@ const int MAX_SPEED = 400;
 const int CALIBRATION_SPEED = 150;
 
 void setup() {
-  reflectanceSensors.init();
   button.waitForButton();
+  pinMode(13, OUTPUT);
+  
+  
+  reflectanceSensors.init();
   motors.setSpeeds(0,0);
   Serial.begin(9600);
 
-  motors.setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);
-  
+  motors.setSpeeds(CALIBRATION_SPEED, -CALIBRATION_SPEED);  
   for (int i = 0; i < 90; i++) {  
     reflectanceSensors.calibrate();
   }
@@ -31,5 +33,28 @@ void setup() {
 void loop() {
   unsigned int sensors[6];
   int position = reflectanceSensors.readLine(sensors);
-  Serial.println(String(sensors[0]));
+
+  if (onLine(sensors)) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+  
+  
+  int error = position - 2500;
+  
+  // motors.setSpeeds(m1Speed, m2Speed);
 }
+
+bool onLine(unsigned int sensors[])  {
+  if(sensors[0] > 500
+    || sensors[1] > 500
+    || sensors[2] > 500
+    || sensors[3] > 500
+    || sensors[4] > 500
+    || sensors[5] > 500) {
+      return true;
+    }
+    return false;
+}
+
